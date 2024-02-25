744020d275887296ca4153db9acec2755e62a2f5
TC303 <toddc0505@gmail.com>
2024-02-24 18:22:23 -0600

    First commit

---
+++ TimeProcess.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Name: Todd Mizera
+#Date:  1/6/2024
+import time    # use time library to time the code executions
+
+# get current time before the process
+start_time = time.time()
+
+# run the process
+for i in range(1000):
+    print( "Hello Everyone!" )
+
+# get current time after the process
+end_time = time.time()
+
+# subtract start time from end time to get time used by process
+total_time = end_time - start_time
+
+# Show the result.  Note: .6f means “show six decimal places”
+print("\nSeconds run 1000 times: {:.6f}".format(total_time))  
